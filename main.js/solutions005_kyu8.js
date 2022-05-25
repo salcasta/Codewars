9d8d314ba8263ad1793aade719bc23d28d869013
salcasta <57605337+salcasta@users.noreply.github.com>
2022-05-25 13:42:16 -0500

    Reversed Strings

--- main.js/solutions005_kyu8.js
+++ main.js/solutions005_kyu8.js
@@ -1,3 +1,4 @@
+//Reversed Strings
 //Complete the solution so that it reverses the string passed into it.
 
 //'world'  =>  'dlrow'
@@ -5,6 +6,6 @@
 
 function solution(str){
     return str.split("").reverse().join("");
-  }
+}
 
   
