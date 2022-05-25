c998c371dca371bb54d114dc754a1e9f656e737d
salcasta <57605337+salcasta@users.noreply.github.com>
2022-05-25 13:39:42 -0500

    Convert a Number to a String

--- main.js/solutions003_kyu8.js
+++ main.js/solutions003_kyu8.js
@@ -1,11 +1,8 @@
+//Convert a Number to a String
 //We need a function that can transform a number into a string.
-
-function numberToString(num) {
-
-}
 
 
 function numberToString(num) {
     return num.toString()
-  }
+}
 
