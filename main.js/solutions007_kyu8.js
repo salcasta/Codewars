ac3ee46ee9c65f3dfb31b6ea612b0b995ab7d31d
salcasta <57605337+salcasta@users.noreply.github.com>
2022-05-25 13:45:01 -0500

    Return Negative

--- main.js/solutions007_kyu8.js
+++ main.js/solutions007_kyu8.js
@@ -1,3 +1,4 @@
+//Return Negative
 //In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
 //Examples
@@ -12,6 +13,6 @@ function makeNegative(num) {
       return -num
     } else 
       return num
-  }
+}
 
-  
+ 
