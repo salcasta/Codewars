df1c8b45e26f0da2f3730c89ea8714f88db1b6df
salcasta <57605337+salcasta@users.noreply.github.com>
2022-06-27 20:32:31 -0500

    Beginner - Lost Without a Map

---
+++ main.js/solutions025_kyu8.js
@@ -0,0 +1,14 @@
+//Beginner - Lost Without a Map
+
+
+//Given an array of integers, return a new array with each value doubled.
+
+//For example:
+
+//[1, 2, 3] --> [2, 4, 6]
+
+
+function maps(x){
+    let doubleX = x.map(x => 2*x)
+    return doubleX
+}
