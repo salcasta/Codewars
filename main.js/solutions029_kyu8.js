206f15d48a515a90ca753bf8ce58782ed19856c2
salcasta <57605337+salcasta@users.noreply.github.com>
2022-06-30 14:30:33 -0500

    Calculate Average

---
+++ main.js/solutions029_kyu8.js
@@ -0,0 +1,17 @@
+//Calculate average
+
+//Write a function which calculates the average of the numbers in a given list.
+
+//Note: Empty arrays should return 0.
+
+function find_average(array) {
+    if (array.length === 0) {
+    return 0;
+    }
+    let result = 0;
+    for (i = 0; i < array.length; i++) {
+      result += array[i];
+    }
+    return result/array.length;
+}
+
