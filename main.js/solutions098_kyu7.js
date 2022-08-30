02800d28a72754a160e39bf44c420535a8cc2676
salcasta <57605337+salcasta@users.noreply.github.com>
2022-08-30 15:42:55 -0500

    String ends with?

---
+++ main.js/solutions098_kyu7.js
@@ -0,0 +1,25 @@
+//String ends with?
+
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+function solution (str, ending) {
+    let stepOne = str.slice((str.length - ending.length))
+    if (stepOne == ending) {
+      return true
+    } else {
+      return false
+    }
+}
+  
+//OR
+  
+function solution(str, ending){
+    return str.endsWith(ending);
+}
