5dee05ca7670cae12d53efc9a85f61203284da8b
salcasta <57605337+salcasta@users.noreply.github.com>
2022-09-08 18:08:36 -0500

    Sort arrays alphabetically

---
+++ main.js/solutions106_kyu7.js
@@ -0,0 +1,9 @@
+//Sort arrays alphabetically
+
+
+//Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+
+sortme = function( names ){
+  return names.sort()
+}
