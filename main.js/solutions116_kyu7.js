ca9257dc1b4df9847d3aa02de2efc381be35c7b9
salcasta <57605337+salcasta@users.noreply.github.com>
2022-09-28 23:31:55 -0500

    Fizz Buzz

---
+++ main.js/solutions116_kyu7.js
@@ -0,0 +1,37 @@
+//Fizz Buzz
+
+/*
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+N will never be less than 1.
+
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+*/
+
+function fizzify(i) {
+    if (i % 15 == 0)
+      return 'FizzBuzz';
+    else if (i % 5 == 0)
+      return 'Buzz';
+    else if (i % 3 == 0)
+      return 'Fizz';
+    else
+      return i;
+}
+  
+// Return an array
+
+function fizzbuzz(n) {
+    let result = []
+    
+    for (var i = 1; i <= n; ++i) result.push(fizzify(i))
+    
+    return result
+}
