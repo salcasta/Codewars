23baae8641a86797b5d059c0d65d5a08da14d43f
salcasta <57605337+salcasta@users.noreply.github.com>
2022-11-01 15:50:47 -0500

    String transformer

---
+++ main.js/solutions133_kyu6.js
@@ -0,0 +1,37 @@
+//String transformer
+
+
+/*
+Given a string, return a new string that has transformed based on the input:
+
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input.
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE"
+You may assume the input only contain English alphabet and spaces.
+*/
+
+function stringTransformer(str) {
+  
+    let opposite = str.split('').map(item => item === item.toUpperCase() ? item.toLowerCase() : item.toUpperCase()).join('')
+  
+    return opposite.split(' ').reverse().join(' ')
+}
+
+
+//OR
+
+function stringTransformer(str) {
+    return str
+      .split(' ')
+      .reverse()
+      .join(' ')
+      .split('')
+      .map(v => v == v.toUpperCase() ?
+        v.toLowerCase() :
+        v.toUpperCase())
+      .join('');
+}
