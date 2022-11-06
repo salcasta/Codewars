5996cd625d45625496c8c63366666fa61e71f676
salcasta <57605337+salcasta@users.noreply.github.com>
2022-11-05 20:27:05 -0500

    Find the capitals

---
+++ main.js/solutions137_kyu7.js
@@ -0,0 +1,19 @@
+// Find the capitals
+
+
+/*
+Instructions
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+*/
+
+function capitals (str) {
+    let answer = []
+    let arr = str.split('')
+    
+    arr.map((l, i, a) => l === l.toUpperCase() ? answer.push(i) : null )
+    
+    return answer
+}
