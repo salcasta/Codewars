739a3be9bead441c889f5b661e3c117f18f73d49
salcasta <57605337+salcasta@users.noreply.github.com>
2022-11-26 23:51:54 -0600

    Switcheroo

---
+++ main.js/solutions148_kyu7.js
@@ -0,0 +1,12 @@
+//Switcheroo
+
+//Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+//Example:
+
+//'acb' --> 'bca'
+//'aabacbaa' --> 'bbabcabb'
+
+function switcheroo(x){
+    return x.split('').map(v => v == 'a' ? v = 'b' : v == 'b' ? v = 'a' : v).join('')
+}
